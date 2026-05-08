9b9bdd1332426bd56979e5aaf3db79d0ed9a8081
Kavisha <sc23k2g@leeds.ac.uk>
2026-05-08 20:26:43 +0100

    bridge create, image converted and shown - not tested yet

--- ros2_project_sc23k2g/Skeleton_Code_First_Step.py
+++ ros2_project_sc23k2g/Skeleton_Code_First_Step.py
@@ -18,13 +18,29 @@ class colourIdentifier(Node):
     def __init__(self):
         super().__init__('cI')
         
+        self.bridge = CvBridge() #Bridge between ROS2 and OpenCV
         # Remember to initialise a CvBridge() and set up a subscriber to the image topic you wish to use
         # We covered which topic to subscribe to should you wish to receive image data
 
-        self.subscription  # prevent unused variable warning
+        self.subscription = self.create_subscription(
+            Image, #type of message
+            '/camera/image_raw', #type of channel 
+            self.callback,
+            10  # how many messages to queue
+            )  # prevent unused variable warning
         
     def callback(self, data):
-        return
+        try: #incase image conversion is corrupted
+            # Convert the ROS2 image to OpenCV format
+            cv_image = self.bridge.imgmsg_to_cv2(data, 'bgr8') #OpenCV stores Blue, Green, red and each channel uses 8 bits (obv)
+        except CvBridgeError as e:
+            self.get_logger().error(str(e))
+            return
+        
+        # Show the image
+        cv2.imshow('Camera Feed', cv_image) #prepare img to show
+        cv2.waitKey(1) #actually show the image
+    
         # Convert the received image into a opencv image
         # But remember that you should always wrap a call to this conversion method in an exception handler
         # Show the resultant images you have created.
