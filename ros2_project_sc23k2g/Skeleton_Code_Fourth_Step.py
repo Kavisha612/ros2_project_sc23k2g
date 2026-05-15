06667ce7cb150f65e2e0738466679675b750ceea
Kavisha <sc23k2g@leeds.ac.uk>
2026-05-15 21:23:55 +0100

    Added contouring and detection for all colors

--- ros2_project_sc23k2g/Skeleton_Code_Fourth_Step.py
+++ ros2_project_sc23k2g/Skeleton_Code_Fourth_Step.py
@@ -17,119 +17,124 @@ import signal
 class Robot(Node):
     def __init__(self):
         super().__init__('robot')
+        # translates ROS2 image messages into OpenCV images
+        self.bridge = CvBridge()
+        self.sensitivity = 10
+        # Subscribe to camera topic - triggers callback() every new frame
+        self.subscription = self.create_subscription(Image, '/camera/image_raw', self.callback, 10)
+        # Publisher to send movement commands
+        self.publisher = self.create_publisher(Twist, '/cmd_vel', 10)
         
-        # Initialise a publisher to publish messages to the robot base
-        # We covered which topic receives messages that move the robot in the 3rd Lab Session
-
 
         # Initialise any flags that signal a colour has been detected (default to false)
-
-
-        # Initialise the value you wish to use for sensitivity in the colour detection (10 should be enough)
-
-
-        # Initialise some standard movement messages such as a simple move forward and a message with all zeroes (stop)
-
-        # Remember to initialise a CvBridge() and set up a subscriber to the image topic you wish to use
-        self.bridge = CvBridge()
-        self.subscription = self.create_subscription(Image, '/camera/image_raw', self.callback, 10)
-        self.subscription  # prevent unused variable warning
-
+        self.blue_detected = False
+        self.task_complete = False
+        self.blue_contour_detected = False
+     
         # We covered which topic to subscribe to should you wish to receive image data
 
     def callback(self, data):
 
-        # Convert the received image into a opencv image
-        # But remember that you should always wrap a call to this conversion method in an exception handler
-        image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
+        try: #incase image conversion is corrupted
+            # Convert the ROS2 image to OpenCV format
+            image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
+        except CvBridgeError as e:
+            self.get_logger().error(str(e))
+            return
+        
+        # COLOUR RANGES
+        
+        # Greem hue = 60
+        hsv_green_lower = np.array([60 - self.sensitivity, 100, 100])
+        hsv_green_upper = np.array([60 + self.sensitivity, 255, 255])
+        
+        # Blue hue = 120
+        hsv_blue_lower = np.array([120 - self.sensitivity, 100, 100])
+        hsv_blue_upper = np.array([120 + self.sensitivity, 255, 255])
+        
+        # Red wraps around so two ranges needed
+        hsv_red1_lower = np.array([0, 100, 100])
+        hsv_red1_upper = np.array([0 + self.sensitivity, 255, 255])
+        
+        hsv_red2_lower = np.array([180 - self.sensitivity, 100, 100])
+        hsv_red2_upper = np.array([180, 255, 255])
+        
+        # Convert the rgb image into a hsv image
+        hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        
+        # Masks
+        # inRange creates a black/white image where white = colour detected
+        green_mask = cv2.inRange(hsv_image, hsv_green_lower, hsv_green_upper)
+        blue_mask = cv2.inRange(hsv_image, hsv_blue_lower, hsv_blue_upper)
+        red1_mask = cv2.inRange(hsv_image, hsv_red1_lower, hsv_red1_upper)
+        red2_mask = cv2.inRange(hsv_image, hsv_red2_lower, hsv_red2_upper)
+        
+        # combine red masks
+        red_mask = cv2.bitwise_or(red1_mask, red2_mask)
+        
+        # add in blue
+        combined1 = cv2.bitwise_or(red_mask, blue_mask)
+        
+        # add in green
+        combined = cv2.bitwise_or(combined1, green_mask)
+        # bitwise_or means: white if pixel matches ANY of the colours
+
+
+        # Apply combined mask to original image
+        # Result: only red, green, blue pixels visible - everything else bla
+        filtered_image = cv2.bitwise_and(image, image, mask = combined)
+        
+        # Show both windows for the video recording
+        # Camera window shows raw feed, Filtered shows colour detection working
         cv2.namedWindow('camera_Feed',cv2.WINDOW_NORMAL)
-        cv2.imshow('camera_Feed', image)
+        cv2.imshow('Camera', image)
+        cv2.imshow('Filtered', filtered_image)
         cv2.resizeWindow('camera_Feed',320,240)
         cv2.waitKey(3)
         
 
-        # Set the upper and lower bounds for the two colours you wish to identify
-        #hue value = 0 to 179
         
-        #hsv_colour1_lower = np.array([<Hue value> - self.sensitivity, 100, 100])
-        #hsv_colour1_upper = np.array([<Hue value> + self.sensitivity, 255, 255])
-        
-        #hsv_colour2_lower = np.array([<Hue value> - self.sensitivity, 100, 100])
-        #hsv_colour2_upper = np.array([<Hue value> + self.sensitivity, 255, 255])
-
-        # Convert the rgb image into a hsv image
-        Hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-        # Filter out everything but a particular colour using the cv2.inRange() method
-
-
-        # Apply the mask to the original image using the cv2.bitwise_and() method
-        # As mentioned on the worksheet the best way to do this is to bitwise and an image with itself and pass the mask to the mask parameter
-
 
         # Find the contours that appear within the certain colour mask using the cv2.findContours() method
-        # For <mode> use cv2.RETR_LIST for <method> use cv2.CHAIN_APPROX_SIMPLE
-
+        contours, _ = cv2.findContours(blue_mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
         # Loop over the contours
         if len(contours)>0:
-
-            # There are a few different methods for identifying which contour is the biggest
-            # Loop through the list and keep track of which contour is biggest or
-            # Use the max() method to find the largest contour
-            
+            # Get the biggest contour
             c = max(contours, key=cv2.contourArea)
-
-
-            #Check if the area of the shape you want is big enough to be considered
-            # If it is then change the flag for that colour to be True(1)
-            if cv2.contourArea(c) > x: #<What do you think is a suitable area?>
+            # Only count it if area is big enough to be a real object not noise
+            if cv2.contourArea(c) > 500: #<What do you think is a suitable area?>
                 # Alter the value of the flag
-                myColourFlag = True
-
-        #Check if a flag has been set = colour object detected - follow the colour object
-        if self.colour1_flag == 1:
-            if cv2.contourArea(c) > aValue:
-                # Too close to object, need to move backwards
-                # Set a flag to tell the robot to move backwards when in the main loop
-                moveBackwardsFlag = True
+                self.blue_detected = True
                 
-            elif cv2.contourArea(c) < aValue:
-                # Too far away from object, need to move forwards
-                # Set a flag to tell the robot to move forwards when in the main loop
-                moveForwardsFlag = True
-            #else:
+                # Store contour so approach logic can use it to steer and judge distance
+                self.blue_contour = c
                 
+                # Draw bounding box around blue object for video evidence
+                x, y, w, h = cv2.boundingRect(c)
+                cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        # Red contours
+        red_contours, _ = cv2.findContours(red_mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        if len(red_contours) > 0:
+            rc = max(red_contours, key=cv2.contourArea)
+            if cv2.contourArea(rc) > 500:
+                x, y, w, h = cv2.boundingRect(rc)
+                cv2.rectangle(image, (x, y), (x+w, y+h), (0, 0, 255), 2)
+                
+        # Green contours
+        green_contours, _ = cv2.findContours(green_mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        if len(green_contours) > 0:
+            gc = max(green_contours, key=cv2.contourArea)
+            if cv2.contourArea(gc) > 500:
+                x, y, w, h = cv2.boundingRect(gc)
+                cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-            # Be sure to do this for the other colour as well
-            # Setting the flag to detect blue, and stop the turtlebot from moving if blue is detected
+       
 
-
-
-        # Show the resultant images you have created. You can show all of them or just the end result if you wish to.
-
-    def walk_forward(self):
-        #Use what you learnt in lab 3 to make the robot move forwards
-        desired_velocity = Twist()
-
-
-        for _ in range(30):  # Stop for a brief moment
-            self.publisher.publish(desired_velocity)
-            self.rate.sleep()
-
-    def walk_backward(self):
-        # Use what you learnt in lab 3 to make the robot move backwards
-        desired_velocity = Twist()
-
-
-        for _ in range(30):  # Stop for a brief moment
-            self.publisher.publish(desired_velocity)
-            self.rate.sleep()
+   
 
     def stop(self):
         # Use what you learnt in lab 3 to make the robot stop
-        desired_velocity = Twist()
-
-
+        desired_velocity = Twist()  # all zeros by default = stop
         self.publisher.publish(desired_velocity)
 
 # Create a node of your class in the main and ensure it stays up and running
@@ -152,12 +157,6 @@ def main():
 
     try:
         while rclpy.ok():
-            # Publish moves
-            #if found green:
-            #    if robot is too close:
-            #        move robot backward()
-            #    else:
-            #        robot walk forward()
             pass
 
     except ROSInterruptException:
